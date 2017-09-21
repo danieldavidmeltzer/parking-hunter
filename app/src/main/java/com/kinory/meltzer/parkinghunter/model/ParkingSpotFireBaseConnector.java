b7e5851b0a2560ac7309e41ac2c208388da5748a
Gilad Kinory <giladkinory2000@gmail.com>
2017-09-21 13:28:25 +0300

    Added Java 8

--- app/src/main/java/com/kinory/meltzer/parkinghunter/model/ParkingSpotFireBaseConnector.java
+++ app/src/main/java/com/kinory/meltzer/parkinghunter/model/ParkingSpotFireBaseConnector.java
@@ -6,6 +6,21 @@ import java.util.Objects;
  * Created by meltzer on 21/09/2017.
  */
 
-public class ParkingSpotFireBaseConnector extends Object{
+public class ParkingSpotFireBaseConnector extends Object {
+
+    private ParkingSpot parkingSpot;
+
+    public ParkingSpotFireBaseConnector(ParkingSpot parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpot parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
 
 }
