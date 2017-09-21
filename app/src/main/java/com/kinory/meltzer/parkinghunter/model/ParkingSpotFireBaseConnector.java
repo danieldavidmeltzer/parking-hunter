8b812dde41b8b803a8d19a38709e41ffc5b86d1b
Daniel Meltzer <meltzerapps@gmail.com>
2017-09-21 13:22:46 +0300

    fix linguistical grammatical annoying mistake

--- app/src/main/java/com/kinory/meltzer/parkinghunter/model/ParkingSpotFireBaseConnector.java
+++ app/src/main/java/com/kinory/meltzer/parkinghunter/model/ParkingSpotFireBaseConnector.java
@@ -1,9 +1,11 @@
 package com.kinory.meltzer.parkinghunter.model;
 
+import java.util.Objects;
+
 /**
  * Created by meltzer on 21/09/2017.
  */
 
-public class ParkinSpotFirebaseConnector {
-    
+public class ParkingSpotFireBaseConnector extends Object{
+
 }
