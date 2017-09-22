cb3bc4901df8af7c22d6368314a0dced4c5fec9d
Daniel Meltzer <meltzerapps@gmail.com>
2017-09-22 23:34:30 +0300

    sign up start to work

---
+++ app/src/main/java/com/kinory/meltzer/parkinghunter/controller/SignUpVC.java
@@ -0,0 +1,15 @@
+package com.kinory.meltzer.parkinghunter.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kinory.meltzer.parkinghunter.R;
+
+public class SignUpVC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_vc);
+    }
+}
