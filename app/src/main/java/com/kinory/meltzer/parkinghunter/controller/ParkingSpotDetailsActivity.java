b10f6b64e1884651d250505d0e952b985e376d13
Daniel Meltzer <meltzerapps@gmail.com>
2017-09-23 16:46:21 +0300

    adding map cool color

--- app/src/main/java/com/kinory/meltzer/parkinghunter/controller/ParkingSpotDetailsActivity.java
+++ app/src/main/java/com/kinory/meltzer/parkinghunter/controller/ParkingSpotDetailsActivity.java
@@ -1,12 +1,16 @@
 package com.kinory.meltzer.parkinghunter.controller;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.kinory.meltzer.parkinghunter.R;
 
 public class ParkingSpotDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -17,7 +21,7 @@ public class ParkingSpotDetailsActivity extends AppCompatActivity implements OnM
         setContentView(R.layout.activity_parking_spot_details);
         // Gets the map fragment
         MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.fragment3);
+                .findFragmentById(R.id.parkingSpotDetailsActivityMap);
 
         // Sets the callback on the fragment
         mapFragment.getMapAsync(this);
@@ -25,6 +29,18 @@ public class ParkingSpotDetailsActivity extends AppCompatActivity implements OnM
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.style_json));
 
+            if (!success) {
+                Log.e("TAG", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("TAG", "Can't find style. Error: ", e);
+        }
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
     }
 }
